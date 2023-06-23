261d15107de8d61dd6ff947700bb0828e53c514f
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-22 22:26:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import WhatsappClient from "./client.js";
+
+// fix for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const whatsappClient = new WhatsappClient();
+whatsappClient.initialize();
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+let server = null;
+
+app.use(cors());
+
+app.get("/qr", (req, res) => {
+
+  if (whatsappClient.isReady()) {
+    res.status(200).send("Client is ready")
+    return
+  }
+
+  if (whatsappClient.isQRGenerated()) {
+    if (req.query.secret === process.env.REQUEST_QR_SECRET) {
+      res.sendFile("/whatsapp-qr.png", { root: __dirname });
+    } else {
+      res.sendStatus(401);
+    }
+  } else {
+    res.sendStatus(503);
+  }
+
+})
+
+
+app.post("/send", (req, res) => {
+  if (whatsappClient.isReady()) {
+    if (req.query.secret === process.env.SEND_MESSAGE_SECRET) {
+      console.log(req.query)
+      whatsappClient.sendVerificationCode(req.query.number, req.query.code)
+        .then((response) => {
+          res.status(200).send(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.sendStatus(500);
+        });
+    } else {
+      res.sendStatus(401);
+    }
+  } else {
+    res.sendStatus(503);
+  }
+})
+
+server = app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
---
+++ client.js
@@ -0,0 +1,70 @@
+import QRCode from "qrcode";
+import WhatsappWebJS from "whatsapp-web.js";
+const { Client, LocalAuth } = WhatsappWebJS;
+
+class WhatsappClient {
+  constructor() {
+    this.client = new Client({
+      authStrategy: new LocalAuth(),
+    });
+    this.qrGenerated = false;
+    this.ready = false;
+  }
+
+  initialize() {
+    this.client.on('qr', (QRText) => {
+      QRCode.toFile('./whatsapp-qr.png', QRText, {
+        errorCorrectionLevel: 'H'
+      }, function (err) {
+        if (err) throw err;
+        console.log('QR Code generated!');
+        this.qrGenerated = true;
+      });
+    });
+
+    this.client.on('ready', () => {
+      console.log('Client is ready!');
+      this.ready = true;
+    });
+
+    this.client.initialize();
+  }
+
+  isReady() {
+    return this.ready;
+  }
+
+  isQRGenerated() {
+    return this.qrGenerated;
+  }
+
+  sendMessage(number, message) {
+    return new Promise((resolve, reject) => {
+      const fixedNumber = number + '@c.us';
+      this.client.isRegisteredUser(fixedNumber)
+        .then((isRegistered) => {
+          if (isRegistered) {
+            this.client.sendMessage(fixedNumber, message)
+              .then((response) => {
+                resolve(response);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else {
+            reject("User is not registered");
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  sendVerificationCode(number, code) {
+    const message = `Bienvenido a Cuidemos el Agua.\n\nTu código de verificación es: *${code}*\n\nSi no solicitaste este código, ignora este mensaje.`
+    return this.sendMessage(number, message)
+  }
+}
+
+export default WhatsappClient;
